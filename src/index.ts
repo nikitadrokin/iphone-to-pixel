0350cb4d801945ecdd58b3f27c6412f3df061664
Nikita D <dnawastaken@gmail.com>
2026-01-08 15:39:08 -0800

    forcing the app to read homebrew paths

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,15 @@ import { Command } from 'commander';
 import { convert } from './commands/convert.js';
 import { randomize } from './commands/randomize.js';
 
+// Fix for macOS sidecar process not having access to Homebrew paths
+if (process.platform === 'darwin') {
+  const commonPaths = ['/opt/homebrew/bin', '/usr/local/bin'];
+  process.env.PATH = [
+    process.env.PATH,
+    ...commonPaths.filter((p) => !process.env.PATH?.includes(p)),
+  ].join(':');
+}
+
 process.on('SIGINT', () => process.exit(0));
 process.on('SIGTERM', () => process.exit(0));
 
