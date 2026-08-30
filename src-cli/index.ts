80cfe484487492e54306fb869f7817364b72cdd5
Nikita D <dnawastaken@gmail.com>
2026-08-30 14:02:37 -0700

    Release v0.0.26

--- src-cli/index.ts
+++ src-cli/index.ts
@@ -28,7 +28,7 @@ async function main() {
   const program = new Command()
     .name('pb')
     .description('Convert iOS media files for Google Pixel 1 compatibility')
-    .version('0.0.25');
+    .version('0.0.26');
 
   program.addCommand(convert);
   program.addCommand(copy);
